ea0a81ca9120b608c376bd7b73ca9dc41423a716
pedromoreira-estg <pmoreira.estg@gmail.com>
2017-10-02 11:29:37 +0100

    aula de 25.09

---
+++ euro.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chave do Euromilhões</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="euro.css">
+
+	<style type="text/css">
+		
+		/* body {background-color: pink;} */
+		/* ul {background-color: skyblue;} */
+	
+	</style>
+</head>
+
+<body>
+
+<h1>Chaves Ganhadoras</h1>
+<h2>nota: 50% são para o prof.</h2>
+<?php 
+$a = 2;
+for($i = 0; $i < $a ; $i++) {
+	echo "<h3>"."este é o olá número ".$i." de um total de ".$a."</h3>";
+}
+?>
+
+<ul class="numbers">
+	<!-- <li style="color:red;">3</li> -->
+	<li>3</li>
+	<li>5</li>
+	<li>17</li>
+	<li>24</li>
+	<li>43</li>
+</ul>
+<ul class="stars">
+	<li>4</li>
+	<li>8</li>
+</ul>
+
+</body>
+
+</html>
