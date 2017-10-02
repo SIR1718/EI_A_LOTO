864ef184f5c50b48623b4569b86d0f74069c38ba
pedromoreira-estg <pmoreira.estg@gmail.com>
2017-10-02 12:06:59 +0100

    consumo de chaves

--- euro.php
+++ euro.php
@@ -1,3 +1,15 @@
+<?php 
+//$a = 5;
+
+$keyJSON = file_get_contents('http://62.28.241.149/~pmoreira/KEYSERVER/KeyServerJSON.php');
+
+$keyPHP = json_decode($keyJSON);
+
+var_dump($keyJSON);
+echo("<br/>");
+var_dump($keyPHP);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,12 +30,37 @@
 <h1>Chaves Ganhadoras</h1>
 <h2>nota: 50% são para o prof.</h2>
 <?php 
-$a = 2;
+// $a = 20;
+/*
 for($i = 0; $i < $a ; $i++) {
 	echo "<h3>"."este é o olá número ".$i." de um total de ".$a."</h3>";
 }
+*/
 ?>
 
+<div>
+<ul class="numbers">
+<?php  
+
+for ($i=0; $i < count($keyPHP->numbers); $i++) {
+	echo "<li>".$keyPHP->numbers[$i]."</li>";
+}
+
+?>
+</ul>
+<ul class="stars">
+
+<?php  
+
+foreach ($keyPHP->stars as $star) {
+	echo "<li>".$star."</li>";
+}
+
+?>
+</ul>
+</div>
+
+<div>
 <ul class="numbers">
 	<!-- <li style="color:red;">3</li> -->
 	<li>3</li>
@@ -36,6 +73,7 @@ for($i = 0; $i < $a ; $i++) {
 	<li>4</li>
 	<li>8</li>
 </ul>
+</div>
 
 </body>
 
